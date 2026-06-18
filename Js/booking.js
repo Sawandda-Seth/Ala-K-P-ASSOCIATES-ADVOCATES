039e8940dea9149abc935e851d649d74b3538a48
Sawandda-Seth <sawandawork@gmail.com>
2026-06-18 13:36:42 +0300

    update booking.js file

--- Js/booking.js
+++ Js/booking.js
@@ -1,1 +1,36 @@
+const consultationForm =
+document.getElementById(
+    "consultationForm"
+);
+
+consultationForm.addEventListener("submit",
+    function (e){
+
+        e.preventDefault();
+        const appointment ={
+            id: Date.now(),
+            name: document.getElementById("clientName").value,
+            email:document.getElementById("clientPhone").value,
+            practiceArea: document.getElementById("practiceArea").value,
+            appointmentDate: document.getElementById("appointDate").value,
+            legalIssue: document.getElementById("legalIssue").value,
+            createdAt: new Date().toLocaleDateString()
+        };
+        const appointments =
+        JSON.parse(
+            localStorage.getItem("appointments")
+        )|| [];
+
+        appointments.push(
+            appointment
+        );
+
+        localStorage.setItem("appointmets",
+            JSON.stingify(appointments)
+        );
+
+        alert("Consultation booked successfully.");
+        consultationForm.reset();
+    }
+);
 

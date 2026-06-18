3333a5ecf386bc6da738939a92e40d9f05f31ced
Sawandda-Seth <sawandawork@gmail.com>
2026-06-18 13:15:24 +0300

    linnking contact.js to contact.html

---
+++ Js/contact.js
@@ -0,0 +1,39 @@
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const subject = document.getElementById("subject").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if(!name || !email|| !subject || !message) {
+        alert("Please complece all fields.");
+        return;
+    }
+
+    const contactData = {
+        id: Date.now(),
+        name,
+        email,
+        subject,
+        message,
+        createdAt: new Date().toLocaleString()
+    };
+
+    const messages =
+    JSON.parse(
+        localStorage.getItem("message")) || [];
+
+        message.push(contactData);
+        localStorage.setItem(
+            "message",
+            JSON.stringify(messages)
+        );
+
+        alert("Message sent successfully.");
+        contactForm.reset();
+
+});
